ab184e6a20614beee165c8589e78882a5610b234
AliaK77 <aliak77@gmail.com>
2026-06-24 23:32:26 +1000

    debug=true REQ & comment

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from project import create_app
 
 
 if __name__=='__main__': 
-    app=create_app() 
+    app=create_app()
+
+    ### debug=True for dev only, set to False for final E2E testing & production
+    ### REQ: #33 07TECHTOOLS app.debug must be set False
     app.run(debug=True) 
     
