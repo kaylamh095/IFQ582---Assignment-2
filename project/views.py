d9074f4a09e01a301505d6d341af340a0a3d7c9e
AliaK77 <aliak77@gmail.com>
2026-06-27 19:17:27 +1000

    added scratch for kath testing

--- project/views.py
+++ project/views.py
@@ -2,7 +2,9 @@
 ### import flask and blueprint / route template
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .forms import RegisterForm
-from .db.user import check_for_user, add_user
+from .db import check_for_user, add_user
+from . import mysql
+
 
 bp = Blueprint('bp', __name__) 
 
@@ -48,3 +50,18 @@ def assessment():
             .format(request.values.get('reviewNotes'), request.values.get('reviewOutcome'), request.values.get('sensitivityLevel'), request.values.get('conditionsofUse'), request.values.get('reviewerName')))
 
     return render_template('assessment.html')
+
+
+
+### scratch testing page
+### @app.route('/scratch')
+@bp.route('/scratch')
+def scratch():
+    cur = mysql.connection.cursor() ### open a cursor to the db
+    ###cur.execute("SELECT * FROM collection_items") ### run the query
+    cur.execute("SELECT title FROM collection_items") ### run the query
+    results = cur.fetchall() ### get all query results
+    cur.close() ### close the cursor
+    ###return render_template('scratch.html', scratch=results) ### pass the data to the template to display
+    return str(results) ### return the results as a string to the browser for initial testing -- worked OK
+
