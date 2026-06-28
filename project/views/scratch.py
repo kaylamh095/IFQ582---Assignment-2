2fa3d0e40a99aa366d4f09ee271d0a99e8f6369b
AliaK77 <aliak77@gmail.com>
2026-06-28 14:56:33 +1000

    scratch - showing item query results

--- project/views/scratch.py
+++ project/views/scratch.py
@@ -9,9 +9,11 @@ bp = Blueprint('scratch', __name__)
 @bp.route('/scratch')
 def scratch():
     cur = cursor() ### open a cursor to the db
-    ###cur.execute("SELECT * FROM collection_items") ### run the query
-    cur.execute("SELECT title FROM collection_items") ### run the query
+    #cur.execute("SELECT * FROM collection_items where item_id = 1") ### run the query
+    #cur.execute("SELECT item_id, title, description, image_link, item_category, cultural_group, sensitivity_notes, review_status, access_level FROM collection_items WHERE item_id = 1") ### run the query
+    cur.execute("SELECT item_id, title, description, image_link, item_category, cultural_group, sensitivity_notes, review_status, access_level FROM collection_items") ### run the query
+    #cur.execute("SELECT title FROM collection_items") ### run the query
     results = cur.fetchall() ### get all query results
     cur.close() ### close the cursor
-    #return render_template('scratch.html', scratch=results) ### pass the data to the template to display
-    return str(results) ### return the results as a string to the browser for initial testing -- worked OK
+    return render_template('scratch.html', data=results) ### pass the data to the template to display
+    #return str(results) ### return the results as a string to the browser for initial testing -- worked OK
